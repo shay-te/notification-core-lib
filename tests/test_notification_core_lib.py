da9d82c7ee52f62bef8e62f98eb6b9c7e4d6bf0c
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2024-09-02 21:03:02 +0300

    Update test_notification_core_lib.py

--- tests/test_notification_core_lib.py
+++ tests/test_notification_core_lib.py
@@ -21,7 +21,7 @@ class TestNotification(unittest.TestCase):
         self.notification_core_lib.notification.create('enjoy our new feature', {'type': 10, 'user_reference_id': '2', 'user_reference_id_2': '44', 'content': ' when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}, project_id)
         self.notification_core_lib.notification.create('user 123user change his name', {'type': 10, 'user_reference_id': '2', 'user_reference_id_2': '44', 'content': ' when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}, project_id)
         self.notification_core_lib.notification.create('some notification', {'type': 10, 'user_reference_id': '2', 'user_reference_id_2': '44', 'content': ' when an unknowt to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}, project_id)
-
+        self.notification_core_lib.notification.create('', {'type': 10, 'user_reference_id': '2', 'user_reference_id_2': '44', 'content': ' when an unknowt to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'}, None)
 
         self.assertEqual(len(self.notification_core_lib.notification.all(user_id_1, {}, couple_id)), 1)
         self.assertEqual(len(self.notification_core_lib.notification.all(user_id_1, {'a': 1}, couple_id)), 0)
